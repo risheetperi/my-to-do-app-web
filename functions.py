5c898fa18511c18f02e69f495cab4b36ae24cdc2
Risheet <risheet.peri@gmail.com>
2023-02-02 15:13:33 +0530

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return the list of
+    to-do Items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """
+    Write a list of to-do items to a text file.
+    """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Test for functions")
